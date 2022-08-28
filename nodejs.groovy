a10da484c5121e9cdfc81b6e84a7c3034b97e5d8
Hyojeong <hyojeong@dable.io>
2022-08-28 17:07:12 +0900

    update: dockerBuildAndPublish

--- nodejs.groovy
+++ nodejs.groovy
@@ -1,18 +1,26 @@
 job('NodeJS example') {
-    scm {
-        git('https://github.com/hyojeong-jang/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('hyojeong@dable.io')
-        }
+  scm {
+    git('https://github.com/hyojeong-jang/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+      node / gitConfigName('DSL User')
+      node / gitConfigEmail('hyojeong@dable.io')
+      }
     }
-    triggers {
-        scm('H/5 * * * *')
+  triggers {
+    scm('H/5 * * * *')
+  }
+  wrappers {
+    nodejs('node16') // this is the name of the NodeJS installation in Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
     }
-    wrappers {
-        nodejs('nodejs') // this is the name of the NodeJS installation in 
-                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+  steps {
+    dockerBuildAndPublish {
+      repositoryName('helloworld')
+      tag('${GIT_REVISION,length=7}')
+      registryCredentials('ecr')
+      dockerRegistryURL('116927014662.dkr.ecr.ap-northeast-2.amazonaws.com/helloworld')
+      forcePull(false)
+      forceTag(false)
+      createFingerprints(false)
+      skipDecorate()
     }
-    steps {
-        shell("npm install")
-    }
+  }
 }
