78ff71c4afce43f2ac8c28fb59d5b7dcb7c8ebc3
Hyojeong <hyojeong@dable.io>
2022-08-28 18:31:35 +0900

    modify: job name

--- nodejs.groovy
+++ nodejs.groovy
@@ -1,4 +1,4 @@
-job('NodeJS example') {
+job('NodeJsExample') {
   scm {
     git('https://github.com/hyojeong-jang/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
       node / gitConfigName('DSL User')
